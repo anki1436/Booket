14ad72271d8a98a522e8cfe154b4a73439e43393
anki1436 <ankitbisht.neosoft@gmail.com>
2022-03-21 11:18:54 +0530

    first commit

---
+++ js/data.js
@@ -0,0 +1,214 @@
+export const categories = [{
+    id: 1,
+    img: "./image/music-event.svg",
+    alt: "music",
+    title: "Music",
+    event: "24 Events Nearby"
+},
+{
+    id: 2,
+    img: "./image/bussiness-event.svg",
+    alt: "bussiness and startup",
+    title: "Business & Startups",
+    event: "24 Events Nearby"
+},
+{
+    id: 3,
+    img: "./image/art-event.svg",
+    alt: "art and culture",
+    title: "Arts & Culture",
+    event: "24 Events Nearby"
+},
+{
+    id: 4,
+    img: "./image/film-event.svg",
+    alt: "film and media",
+    title: "Film & Media",
+    event: "24 Events Nearby"
+},
+{
+    id: 4,
+    img: "./image/food-event.svg",
+    alt: "food and drink",
+    title: "Food & Drink",
+    event: "24 Events Nearby"
+}
+
+
+];
+
+
+
+export const popular = [{
+    id: 1,
+    img: "./image/event-img.jpg",
+    alt: "popular 1",
+    title: "Great Qatar Food Festival",
+    location: "Mosman Park, Sydney",
+    eventTime:"Sun, Nov 15, 11:00 AM"
+},
+{
+    id: 2,
+    img: "./image/event-img.jpg",
+    alt: "popular 2",
+    title: "QAWARIB Qatar International Boat Show 2021",
+    location: "The Pearl Qatar",
+    eventTime:"Sun, Nov 15, 11:00 AM",
+},
+{
+    id: 3,
+    img: "./image/event-img.jpg",
+    alt: "popular 3",
+    title: "Commercial Bank CHI Al SHAQAB Presented by Longines",
+    location: "Al Shaqab",
+    eventTime:"Sun, Nov 15, 11:00 AM",
+},
+{
+    id: 4,
+    img: "./image/event-img.jpg",
+    alt: "popular 4",
+    title: "Tasty Street Qatar at Katara Cultural Village",
+    location: "Al Gannas ",
+    eventTime:"Sun, Nov 15, 11:00 AM",
+}
+
+];
+
+export const nearBy = [{
+    id: 1,
+    img: "./image/event-img.jpg",
+    alt: "nearBy 1",
+    title: "HOT AIR BALLOON  FLIGHTS OVER THE DOHA",
+    location: "Doha",
+    eventTime:"Sun, Nov 15, 11:00 AM"
+},
+{
+    id: 2,
+    img: "./image/event-img.jpg",
+    alt: "nearBy 2",
+    title: "Full Day Desert Safari",
+    location: "Doha, Qatar",
+    eventTime:"Sun, Nov 15, 11:00 AM",
+},
+{
+    id: 3,
+    img: "./image/event-img.jpg",
+    alt: "nearBy 3",
+    title: "North of Qatar",
+    location: "Doha, Qatar",
+    eventTime:"Sun, Nov 15, 11:00 AM",
+},
+{
+    id: 4,
+    img: "./image/event-img.jpg",
+    alt: "nearBy 4",
+    title: "Qatar History vs Future",
+    location: "Doha, Qatar",
+    eventTime:"Sun, Nov 15, 11:00 AM",
+}
+
+];
+export const recomended = [{
+    id: 1,
+    img: "./image/event-img.jpg",
+    alt: "recomended 1",
+    title: "The International  Handicrafts & Traditional Productions Cultural ..",
+    location: "Mosman Park, Sydney",
+    eventTime:"Sun, Nov 15, 11:00 AM"
+},
+{
+    id: 2,
+    img: "./image/event-img.jpg",
+    alt: "recomended 2",
+    title: "International Horticultural Expo Doha",
+    location: "The Pearl Qatar",
+    eventTime:"Sun, Nov 15, 11:00 AM",
+},
+{
+    id: 3,
+    img: "./image/event-img.jpg",
+    alt: "recomended 3",
+    title: "Torba Farmers Market  2020",
+    location: "Doha, Qatar",
+    eventTime:"Ceremonial Court, Education City",
+},
+{
+    id: 4,
+    img: "./image/event-img.jpg",
+    alt: "recomended 4",
+    title: "Sea Caves Water Park",
+    location: "Desert Falls Water and Adventure park",
+    eventTime:"Sun, Nov 15, 11:00 AM",
+}
+
+];
+export const featured = [{
+    id: 1,
+    img: "./image/event-img.jpg",
+    alt: "featured 1",
+    title: "Qatar Museum Tour",
+    location: " Doha, Qatar",
+    eventTime:"Sun, Nov 15, 11:00 AM"
+},
+{
+    id: 2,
+    img: "./image/event-img.jpg",
+    alt: "featured 2",
+    title: "Longines Global Champions Tour - Doha",
+    location: "Blue Mountains, NSW",
+    eventTime:"Sun, Nov 15, 11:00 AM",
+},
+{
+    id: 3,
+    img: "./image/event-img.jpg",
+    alt: "featured 3",
+    title: "Qatar Sports and Educational Tour",
+    location: "Surry Hills, Qatar",
+    eventTime:"Ceremonial Court, Education City",
+},
+{
+    id: 4,
+    img: "./image/event-img.jpg",
+    alt: "featured 4",
+    title: "Shaikh faisal Camal Race track",
+    location: "Surry Hills, Qatar",
+    eventTime:"Sun, Nov 15, 11:00 AM",
+}
+];
+
+export const organizer = [{
+    id: 1,
+    img: "./image/organizer-logo-1.png",
+    alt: "organizer logo-1",
+    title: "Qatar Green Leaders",
+    followerCount: 273
+},
+{
+    id: 2,
+    img: "./image/organizer-logo-1.png",
+    alt: "organizer logo-2",
+    title: "MOC Doha 2021 Managing...s  ",
+    followerCount: 273
+},
+{
+    id: 3,
+    img: "./image/organizer-logo-1.png",
+    alt: "organizer logo-3",
+    title: "Waxdale Certified Launcher Program",
+    followerCount: 273
+},
+{
+    id: 4,
+    img: "./image/organizer-logo-1.png",
+    alt: "organizer logo-4",
+    title: "CH2 Agency Bangalore",
+    followerCount: 273
+},
+{
+    id: 5,
+    img: "./image/organizer-logo-1.png",
+    alt: "organizer logo-5",
+    title: "CH2 Agency Bangalore",
+    followerCount: 273
+}
+];
---
+++ js/main.js
@@ -0,0 +1,177 @@
+
+import { categories }  from "./data.js";
+ import { popular }  from  "./data.js";
+ import { recomended }  from  "./data.js";
+ import { featured }  from  "./data.js";
+ import { nearBy }  from  "./data.js";
+ import { organizer }  from  "./data.js";
+
+categories.forEach((cat) => {
+    let category= document.getElementById("category");
+
+    category.innerHTML += `
+    <div class="catergory-card rounded-lg text-white position-relative">
+    <div class="catergory-card-img position-absolute">
+        <img src= ${cat.img} alt=${cat.alt}>
+    </div>
+    <div class="catergory-card-info">
+        <div class="catergory-card-info-title">
+            ${cat.title}
+        </div>
+        <span class="catergory-card-info-desc">
+        ${cat.event}
+        </span>
+    </div>
+    <div class="catergory-card-btn rounded-pill d-flex justify-content-center align-items-center bg-white">
+        <svg xmlns="http://www.w3.org/2000/svg" width="16.195" height="11.436"
+            viewBox="0 0 16.195 11.436">
+            <path id="Path_63" data-name="Path 63"
+                d="M11.06,14.831a.576.576,0,1,0-.811.811l4.139,4.144H.706a.578.578,0,0,0,0,1.154H14.388l-4.146,4.139a.587.587,0,0,0,0,.811.573.573,0,0,0,.811,0l5.128-5.128a.563.563,0,0,0,0-.811Z"
+                transform="translate(-0.159 -14.624)" fill="#fe7700" />
+        </svg>
+    </div>
+</div>
+    `
+
+})
+popular.forEach((pop)=>{
+    let popularCon= document.getElementById("popular");
+    console.log(popularCon);
+
+    popularCon.innerHTML +=`
+    <div class="event-card">
+                    <div class="event-card-img-con">
+                        <img src=${pop.img} alt=${pop.alt}>
+                        <div class="event-card-img-con-heart-icon rounded-pill rounded-pill">
+                            <img src="./image/event-heart.svg" alt="like">
+                        </div>
+                    </div>
+                    <div class="event-card-desc">
+                        <p class="event-card-desc-time">${pop.eventTime}</p>
+                        <div class="event-card-desc-info">
+                            <div class="event-card-desc-info__left">
+                                <span class="event-card-desc-info__left-title">${pop.title}</span>
+                                <span class="event-card-desc-info__left-location">${pop.location}</span>
+                            </div>
+                            <div class="event-card-desc-info__right">
+                                <div class="event-card-desc-info__right-icon">
+                                    <img src="./image/share.svg" alt="share">
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+                </div>
+    `
+
+});
+featured.forEach((feature)=>{
+    let featuredCon= document.getElementById("featured");
+    console.log(featuredCon);
+
+
+    featuredCon.innerHTML +=`
+    <div class="event-card">
+                    <div class="event-card-img-con">
+                        <img src=${feature.img} alt=${feature.alt}>
+                        <div class="event-card-img-con-heart-icon rounded-pill rounded-pill">
+                            <img src="./image/event-heart.svg" alt="like">
+                        </div>
+                    </div>
+                    <div class="event-card-desc">
+                        <p class="event-card-desc-time">${feature.eventTime}</p>
+                        <div class="event-card-desc-info">
+                            <div class="event-card-desc-info__left">
+                                <span class="event-card-desc-info__left-title">${feature.title}</span>
+                                <span class="event-card-desc-info__left-location">${feature.location}</span>
+                            </div>
+                            <div class="event-card-desc-info__right">
+                                <div class="event-card-desc-info__right-icon">
+                                    <img src="./image/share.svg" alt="share">
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+                </div>
+    `
+
+});
+recomended.forEach((rec)=>{
+    let recCon= document.getElementById("recomended");
+    console.log(recCon);
+
+    recCon.innerHTML +=`
+    <div class="event-card">
+                    <div class="event-card-img-con">
+                        <img src=${rec.img} alt=${rec.alt}>
+                        <div class="event-card-img-con-heart-icon rounded-pill rounded-pill">
+                            <img src="./image/event-heart.svg" alt="like">
+                        </div>
+                    </div>
+                    <div class="event-card-desc">
+                        <p class="event-card-desc-time">${rec.eventTime}</p>
+                        <div class="event-card-desc-info">
+                            <div class="event-card-desc-info__left">
+                                <span class="event-card-desc-info__left-title">${rec.title}</span>
+                                <span class="event-card-desc-info__left-location">${rec.location}</span>
+                            </div>
+                            <div class="event-card-desc-info__right">
+                                <div class="event-card-desc-info__right-icon">
+                                    <img src="./image/share.svg" alt="share">
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+                </div>
+    `
+
+});
+nearBy.forEach((nearBy)=>{
+    let nearByCon= document.getElementById("nearBy");
+
+    nearByCon.innerHTML +=`
+    <div class="event-card">
+                    <div class="event-card-img-con">
+                        <img src=${nearBy.img} alt=${nearBy.alt}>
+                        <div class="event-card-img-con-heart-icon rounded-pill rounded-pill">
+                            <img src="./image/event-heart.svg" alt="like">
+                        </div>
+                    </div>
+                    <div class="event-card-desc">
+                        <p class="event-card-desc-time">${nearBy.eventTime}</p>
+                        <div class="event-card-desc-info">
+                            <div class="event-card-desc-info__left">
+                                <span class="event-card-desc-info__left-title">${nearBy.title}</span>
+                                <span class="event-card-desc-info__left-location">${nearBy.location}</span>
+                            </div>
+                            <div class="event-card-desc-info__right">
+                                <div class="event-card-desc-info__right-icon">
+                                    <img src="./image/share.svg" alt="share">
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+                </div>
+    `
+
+});
+organizer.forEach((organizer)=>{
+    let organizerCon= document.getElementById("organizer");
+
+    organizerCon.innerHTML +=`
+    <div class="organizer-card">
+    <div class="organizer-card-close">
+        <img src="./image/organizer-close.svg" alt="close">
+    </div>
+    <img src=${organizer.img} class="organizer-card-img" alt=${organizer.alt}>
+    <h5 class="organizer-card-title">${organizer.title}</h5>
+    <span class="organizer-card-follower">${organizer.followerCount}</span>
+    <button class="btn btn-outline-warning organizer-card-btn ">Follow</button>
+</div>
+    `
+
+
+});
